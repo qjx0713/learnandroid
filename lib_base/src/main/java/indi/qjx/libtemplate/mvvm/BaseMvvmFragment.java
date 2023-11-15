e52923ea01df0e7fa7cf90ab111786a88d32c794
qianjinxing <qianjx1@chinatelecom.cn>
2023-11-15 17:55:44 +0800

    branch template

---
+++ lib_base/src/main/java/indi/qjx/libtemplate/mvvm/BaseMvvmFragment.java
@@ -0,0 +1,98 @@
+package indi.qjx.libtemplate.mvvm;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.lang.reflect.ParameterizedType;
+
+
+public abstract class BaseMvvmFragment<VM extends ViewModel & DefaultLifecycleObserver, DB extends ViewDataBinding> extends Fragment {
+    protected VM mViewModel;
+    protected DB mViewDataBind;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        if (args != null) {
+            handleArguments(args);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mViewDataBind = DataBindingUtil.inflate(inflater, getLayoutId(), container, false);
+
+        initCommonView();
+        initViewModel();
+        bindViewModel();
+
+        init();
+
+        // 让 ViewModel 可以感知 Fragment 的生命周期
+        if (mViewModel != null) {
+            getLifecycle().addObserver(mViewModel);
+        }
+
+        return mViewDataBind.getRoot();
+    }
+
+    /**
+     * 处理参数
+     *
+     * @param args 参数容器
+     */
+    protected void handleArguments(Bundle args) {
+
+    }
+
+    /**
+     * 获取当前页面的布局资源ID
+     *
+     * @return 布局资源ID
+     */
+    protected abstract int getLayoutId();
+
+    /**
+     * 用于初始化通用view，比如AppBar
+     */
+    protected void initCommonView() {
+
+    }
+
+    /**
+     * 初始化ViewModel
+     */
+    protected void initViewModel() {
+        //获得泛型参数的实际类型
+        Class<VM> vmClass = (Class<VM>) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+        mViewModel = new ViewModelProvider(this).get(vmClass);
+    }
+
+    /**
+     * 绑定ViewModel
+     */
+    protected abstract void bindViewModel();
+
+    /**
+     * 初始化
+     */
+    protected abstract void init();
+}
